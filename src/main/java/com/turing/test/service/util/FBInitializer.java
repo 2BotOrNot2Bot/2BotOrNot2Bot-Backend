3e881b38fff0a938d8efea999b4c7a4bff0d05dd
Yibo <yibowen@usc.edu>
2021-11-12 00:12:54 -0800

    RedisLock Implementation

--- src/main/java/com/turing/test/service/util/FBInitializer.java
+++ src/main/java/com/turing/test/service/util/FBInitializer.java
@@ -1,4 +1,4 @@
-package com.turing.test.service.impl;
+package com.turing.test.service.util;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
---
+++ src/main/java/com/turing/test/service/util/RedisLock.java
@@ -0,0 +1,38 @@
+package com.turing.test.service.util;
+
+import com.google.common.base.Strings;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+/**
+* @Author Yibo Wen
+* @Date 11/11/2021 11:29 PM
+**/
+@Slf4j
+@Service
+public class RedisLock {
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    public Boolean tryLock(String lockKey, String clientId, long millis){
+        if(Boolean.TRUE.equals(redisTemplate.opsForValue().setIfAbsent(lockKey, clientId, Duration.ofMillis(millis)))){
+            return true;
+        }
+        log.warn("RedisLock->tryLock: lock error");
+        return false;
+    }
+
+    public Boolean tryUnlock(String lockKey, String clientId){
+        String id = redisTemplate.opsForValue().get(lockKey);
+        if(!Strings.isNullOrEmpty(id) && id.equals(clientId)){
+            return redisTemplate.opsForValue().getOperations().delete(lockKey);
+        }
+        log.warn("RedisLock->tryUnlock: unlock error");
+        return false;
+    }
+}
