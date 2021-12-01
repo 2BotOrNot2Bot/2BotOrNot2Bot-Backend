371c9726bf2df55af5924913d03e8ff92571b21e
Lorenayannnnn <lorenayan@qq.com>
2021-12-01 12:46:19 -0800

    add form

--- src/main/java/com/turing/test/domain/form/UserLoginForm.java
+++ src/main/java/com/turing/test/domain/form/UserLoginForm.java
@@ -2,6 +2,11 @@ package com.turing.test.domain.form;
 
 import lombok.Data;
 
+/**
+ * @Author Tianyi Yan
+ * @Date 2021/12/01 11:35
+ * Form for receiving request parameters containing user id
+ **/
 @Data
 public class UserLoginForm {
     private String uid;
